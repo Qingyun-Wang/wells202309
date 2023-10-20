8d94f46143e5a543adac6fb4019a965a37359f96
Qingyun <qwang63@uic.edu>
2023-10-20 23:31:11 +0000

    wrap code into funtion

--- database.py
+++ database.py
@@ -3,21 +3,24 @@ import os
 
 URL_DB=os.getenv("URL_DB")
 
-engine=create_engine(URL_DB)
 
-conn=engine.connect()
+def query_db(depth_min, grad_min):
+    """return wells based on criteria"""
+    
+    engine=create_engine(URL_DB)
 
-depth_min = 2000
-grad_min = 0.07
+    conn=engine.connect()
 
-query = f"""SELECT latitude, longitude, depth, gradient
-            FROM wells
-            WHERE depth > {depth_min} AND gradient > {grad_min}
-            
-        """
+    query = f"""SELECT latitude, longitude, depth, gradient
+                FROM wells
+                WHERE depth > {depth_min} AND gradient > {grad_min}
 
-results = conn.execute(query).fetchall()
+            """
+    results = conn.execute(query).fetchall()
 
-conn.close()
+    conn.close()
 
-print(results)
+    return results
+
+if __name__ == '__main__':
+    

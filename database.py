933c2039054405ae840a84fb654a23fbc0dd814a
Jane Doe <qwang63@uic.edu>
2023-10-20 21:48:23 +0000

    added first verion of database.py

---
+++ database.py
@@ -0,0 +1,23 @@
+from sqlalchemy import create_engine
+import os
+
+URL_DB=os.getenv("URL_DB")
+
+engine=create_engine(URL_DB)
+
+conn=engine.connect()
+
+depth_min = 2000
+grad_min = 0.07
+
+query = f"""SELECT latitude, longitude, depth, gradient
+            FROM wells
+            WHERE depth > {depth_min} AND gradient > {grad_min}
+            
+        """
+
+results = conn.execute(query).fetchall()
+
+conn.close()
+
+print(results)

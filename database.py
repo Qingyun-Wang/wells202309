9aed9e969844bb60f6245ae61767b6c17d9e605a
Qingyun <qwang63@uic.edu>
2023-10-21 00:43:45 +0000

    add useer input

--- database.py
+++ database.py
@@ -1,27 +1,28 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 import os
 
-URL_DB=os.getenv("URL_DB")
 
+URL_DB = os.getenv("URL_DB")
 
 def query_db(depth_min, grad_min):
-    """return wells based on criteria"""
+    """Return wells based on criteria."""
     
-    engine=create_engine(URL_DB)
+    engine = create_engine(URL_DB)
 
-#    conn=engine.connect()
+    query = text("""
+        SELECT latitude, longitude, depth, gradient
+        FROM wells
+        WHERE depth > :depth_min AND gradient > :grad_min
+    """)
 
-    query = f"""SELECT latitude, longitude, depth, gradient
-                FROM wells
-                WHERE depth > {depth_min} AND gradient > {grad_min}
-
-            """
     with engine.connect() as conn:
-        results = conn.execute(query).fetchall()
+        results = conn.execute(query, depth_min=depth_min, grad_min=grad_min).fetchall()
         
     return results
 
 if __name__ == '__main__':
-    depth_min=2000
-    grad_min=0.1
+    import sys
+
+    depth_min = float(sys.argv[1])
+    grad_min = float(sys.argv[2])
     print(query_db(depth_min, grad_min))
